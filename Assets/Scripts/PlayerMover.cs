ab20b8e7f46684ae20dcdb00425d01a02746e4cf
Renata910 <renata_s_barbosa@estudante.sesisenai.org.br>
2025-06-26 21:19:13 -0300

    renata

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    public float moveSpeed = 9f;
+// modifiquei a velocidade aumentanodo ela pra 9 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.  r ))
+// modifiquei a tecla que realiza a ação para "r"
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
