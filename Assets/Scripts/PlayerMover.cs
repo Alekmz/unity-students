2a31176f5cb7fa19283ed27a7590863ed90c1f6d
PEDRO GABRIEL AMORIM DOS SANTOS <pedro_ga_santos@estudante.sesisenai.org.br>
2025-06-30 21:10:04 -0300

    Adicionei uma variavel nova de gravidade

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public float gravityScale = 4;
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
